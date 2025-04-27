67a8187926fe79d926b99704294c8dfdee7ab81e
Ali Gholipour <Ali1383gho@gmail.com>
2025-04-27 11:47:05 +0330

    final fix

--- src/components/AnimateButton.jsx
+++ src/components/AnimateButton.jsx
@@ -15,7 +15,7 @@ export default function AnimateButton() {
       onClick={handleClick}
       className={`bg-[#8a5fa5] transition-all duration-150 transform ${
         pressed ? "scale-90" : "scale-100"
-      } cursor-pointer rounded-2xl border border-black flex items-center justify-center w-full h-[15vh] min-h-[80px] max-h-[120px] md:h-[20vh] lg:h-[25vh]`}
+      } cursor-pointer rounded-2xl border border-black flex items-center justify-center w-full h-[12vw] min-h-[60px] max-h-[150px] aspect-square`}
     >
       <div className="bg-[#d285cc] w-full h-full rounded-2xl border border-black flex items-center justify-center pb-1 sm:pb-2 md:pb-4">
         <div className="bg-[#fcd9f8] w-full h-full rounded-xl border border-black shadow-md"></div>
