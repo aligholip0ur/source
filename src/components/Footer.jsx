2d7c240490ff1b8a7a0b0d0b465794c87560c3e8
Ali Gholipour <Ali1383gho@gmail.com>
2025-04-29 12:32:54 +0330

    ff4.9

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,17 @@
 export default function Footer() {
-    return (
-      <>
-        <div 
-          className="w-full mx-auto mt-4" 
-          style={{
-            height: "4%",
-            width: "min(80vw, 240vh)", 
-            maxWidth: "1600px", 
-            margin: "0 auto", 
-          }}
-        >
-          <div className="w-full h-full bg-[#cefef4] rounded-full " />
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div 
+        className="w-full mx-auto" 
+        style={{
+          height: "4%",
+          width: "min(90vw, 240vh)", 
+          maxWidth: "1600px", 
+          margin: "0 auto", 
+        }}
+      >
+        <div className="w-full h-full bg-[#cefef4] rounded-full" />
+      </div>
+    </>
+  );
+}
